5f999150c9abf0fda20aaa1e44a24b3572a56083
Kayo Kennedy <Kayo.kennedy@academico.ifpb.edu.br>
2024-10-27 13:00:41 -0300

    Adicionando questão: 04

---
+++ questoes-funcoes/funcoes-questao04.js
@@ -0,0 +1,5 @@
+function palavraPresente(frase, palavra) {
+    return frase.includes(palavra);
+}
+
+console.log(palavraPresente("Aprender JavaScript é divertido", "JavaScript"));  // Retornará true
