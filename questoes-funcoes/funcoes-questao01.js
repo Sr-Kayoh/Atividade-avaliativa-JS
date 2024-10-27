463b00fcdfc9ddb323a9420d3d30372053f648d7
Kayo Kennedy <Kayo.kennedy@academico.ifpb.edu.br>
2024-10-27 12:59:31 -0300

    Adicionando questão: 01

---
+++ questoes-funcoes/funcoes-questao01.js
@@ -0,0 +1,6 @@
+function inverterNumero(numero) {
+    let numeroInvertido = parseInt(numero.toString().split('').reverse().join(''));
+    return numeroInvertido;
+}
+
+console.log(inverterNumero(12356));  
